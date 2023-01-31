9bdce87c6dde831da0d4bd76fa9c724d7f4369eb
Rifqi Subagja <rifqisubagjaa@gmail.com>
2023-01-31 22:52:35 +0700

    add api get user

--- routes/handler/users/index.js
+++ routes/handler/users/index.js
@@ -1,9 +1,11 @@
 const register = require('./register');
 const login = require('./login');
 const update = require('./editprofile');
+const getuser = require('./getuser');
 
 module.exports = {
     register,
     login,
-    update
+    update,
+    getuser
 }
